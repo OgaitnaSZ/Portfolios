c56d9445dccbe27d322be729242fd7421e5a2413
OgaitnaSZ <zanty201212@gmail.com>
2025-06-13 22:28:21 -0300

    Se agrego el efecto de particulas en el Hero. Y se organizaron mejor los assets

---
+++ assets/js/app.js
@@ -0,0 +1,48 @@
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80, density: { enable: true, value_area: 800 } },
+    fpsLimit: 120,
+    color: { value: "#ffffff" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 3, random: true },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+        direction: "none",
+        enable: true,
+        outModes: {
+            default: "bounce",
+        },
+        random: true,
+        speed: 1,
+        straight: false,
+    },
+  },
+   modes: {
+    repulse: {
+        distance: 200,
+        duration: 0.5,
+    },
+    grab: {
+        distance: 150,
+        line_linked: {
+            opacity: 20
+        }
+    },
+},
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "grab" },
+      onclick: { enable: true, mode: "repulse" },
+      resize: true,
+    },
+  },
+  retina_detect: true,
+});
