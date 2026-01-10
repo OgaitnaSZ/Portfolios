5e6b3f22cc61c2a3281e9e9f6e84df9e991294bf
OgaitnaSZ <zanty201212@gmail.com>
2026-01-10 15:29:23 -0300

    preliminar design of header

---
+++ src/app/layout/header/header.ts
@@ -0,0 +1,29 @@
+import { NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [NgClass],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  isDark: boolean = false;
+
+  ngOnInit(): void {
+    // Cargar modo oscuro
+    this.isDark = localStorage.getItem('theme') === 'dark';
+  }
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    const html = document.documentElement;
+    if (this.isDark) {
+      html.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      html.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }
+}
